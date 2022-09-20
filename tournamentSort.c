e23f9c2be14ef3e2b30738ce995ba2b03f15ea14
IzatheGreat15 <20100869@usc.edu.ph>
2022-09-20 16:02:37 +0800

    add time and space complexity

---
+++ tournamentSort.c
@@ -0,0 +1,8 @@
+/**
+ * create another array twice the size of original array
+ * store original array to second half of other array
+ * store index of winner to higher level of tree
+ * replace first index in original array with the winner
+ * define the winner index of the other array as INFINITY
+ * then repeat
+ * **/
