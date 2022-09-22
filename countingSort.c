94cb3a73e567ad845585efc59a91d49f240bcb26
IzatheGreat15 <20100869@usc.edu.ph>
2022-09-22 11:51:03 +0800

    add counting sort negative variation

--- countingSort.c
+++ countingSort.c
@@ -33,10 +33,47 @@ void countingSort(int A[], int size){
   }
 }
 
+void countingSortv2(int A[], int size){
+  int *sorted = (int*)calloc(size, sizeof(int));
+  int x, max = A[0], min = A[0];
+
+  //find the smallest and largest number in the array
+  for(x = 1; x < size; x++){
+    max = (max < A[x]) ? A[x] : max;
+    min = (min > A[x]) ? A[x] : min;
+  }
+
+  min = (min < 0) ? min * -1 : 0;
+  //create count array with length max + 1 + min
+  int *count = (int*)calloc((max + 1) + min, sizeof(int));
+
+  //count each occurense of number
+  for(x = 0; x < size; x++){
+    count[A[x] + min]++;
+  }
+
+  //store cummulative sum
+  for(x = 1; x < max + 1 + min; x++){
+    count[x] += count[x - 1];
+  }
+
+  //place elments in sorted array
+  for(x = 0; x < size; x++){
+    sorted[--count[A[x] + min]] = A[x];
+    // printf(" occ : %d %d ", --count[A[x] + min], A[x]);
+  }
+
+  //transfer sorted array to original array
+  for(x = 0; x < size; x++){
+    A[x] = sorted[x];
+  }
+
+}
+
 int main() {
-  int arr[] = {3, 6, 4, 0, 2, 3};
+  int arr[] = {3, 6, 4, 0, 2, 3, -4};
   int size = sizeof(arr)/sizeof(arr[0]);
-  countingSort(arr, size);
+  countingSortv2(arr, size);
 
   //display sorted array
   int x;
