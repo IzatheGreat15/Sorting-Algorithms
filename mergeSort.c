6370a295e2364733bbd6166db028ef78bf6cfa1b
IzatheGreat15 <20100869@usc.edu.ph>
2022-09-27 15:35:38 +0800

    merge sort bottom up approach

--- mergeSort.c
+++ mergeSort.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+//Top down approach
 void merge(int A[], int L[], int R[], int Lsize, int Rsize){
   int Andx = 0, Lndx, Rndx;
   
@@ -46,11 +48,53 @@ int mergeSort(int arr[], int size){
   merge(arr, L, R, mid, size - mid);
 }
 
+//Bottom up approach
+void mergev2(int A[], int L[], int R[], int first, int second, int F_size, int S_size){
+  int x, y, z;
+  for(x = 0, y = 0, z = first; x < F_size && y < S_size; z++){
+    A[z] = (L[x] <= R[y]) ? L[x++] : R[y++]; 
+  }
+  
+  while(x < F_size){
+    A[z++] = L[x++];
+  }
+  
+  while(y < S_size){
+    A[z++] = R[y++];
+  }
+}
+
+void mergeSortv2(int A[], int size){
+  int x, y;
+  
+  for(x = 1; x <= size; x *= 2){
+    int *L = (int*)calloc(x, sizeof(int));
+    int *R = (int*)calloc(x, sizeof(int));
+    for(y = 0; y + x < size; y += x*2){
+      int first = y;
+      int second = y + x;
+      int ctr, z, F_size = 0, S_size = 0;
+      //create left segment
+      for(ctr = first, z = 0; ctr < size && ctr < second; ctr++, z++){
+        L[z] = A[ctr];
+        F_size++;
+      }
+      //create right segment
+      for(ctr = second, z = 0; ctr < size && ctr < second + x; ctr++, z++){
+        R[z] = A[ctr];
+        S_size++;
+      }
+      //merge
+      mergev2(A, L, R, first, second, F_size, S_size);
+    }
+  }
+}
+
 
 int main() {
-  int arr[] = {3, 6, 4, 0, 2};
+  int arr[] = {3, 6, 4, 0, 2, -1};
   int size = sizeof(arr)/sizeof(arr[0]);
-  mergeSort(arr, size);
+  mergeSortv2(arr, size);
 
   //display sorted array
   int x;
